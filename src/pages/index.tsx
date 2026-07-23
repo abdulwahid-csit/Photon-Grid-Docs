f3f36e8507e2ecaa4669d6afd419eb38c07b01c3
Abdul Wahid <wahid@4iisolutions.com>
2026-07-23 13:04:37 +0500

    data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ function HomepageHeader() {
           pagination, integrated charts, sparklines and theming — with official
           wrappers for React, Angular and Vue.
         </p>
-        <div className={styles.buttons}>
+        <div className={styles.buttons}> 
           <Link
             className="button button--secondary button--lg"
             to="/docs/getting-started/introduction">
